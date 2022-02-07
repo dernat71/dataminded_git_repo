2407830fd9365155ef985a44c0e3c3cb69da84f8
YannickDeBock <yannick_debock@hotmail.com>
2022-02-07 12:41:06 +0000

    Developed a new feature: printing a welcoming statement ('Hello') in setup.py, updated requirements.txt (pandas), changed release version to 0.0.2 in mysupercliproject/version.py

--- mysupercliproject/version.py
+++ mysupercliproject/version.py
@@ -9,4 +9,4 @@ This module contains project version information.
 """
 
 __version__ = "0.0.1"  #: the working version
-__release__ = "0.0.1"  #: the release version
+__release__ = "0.0.2"  #: the release version
